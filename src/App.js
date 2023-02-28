1333a00554830cdeb0e4bdd7ca852bc71d0b91f8
Mike Kubesheski <imkubes1@gmail.com>
2023-02-28 15:45:45 -0600

    second commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Container from "react-bootstrap/Container"
+
+function App() {
+  return <Container>
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budgets</h1>
+    </Stack>
+  </Container>
+}
+
+export default App;
